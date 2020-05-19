fef4402a2dfd9d577f2eda2ba0db04aa0e2b964c
Michael Efseaff <mikefseaff@gmail.com>
2020-05-19 11:55:52 -0700

    pause menu

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused;
+
+    public GameObject pauseMenuCanvas;
+
+    void Update()
+    {
+        if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+    }
+    public void Resume()
+    {
+        isPaused = false;
+    }
+    public void Quit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+    }
+}
