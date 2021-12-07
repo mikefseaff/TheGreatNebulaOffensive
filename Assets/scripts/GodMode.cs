1ec3839f51c264b252313ea0b25cf82ec7e3f1ba
Michael Efseaff <mikefseaff@gmail.com>
2021-12-07 15:11:27 -0800

    godmode added

---
+++ Assets/scripts/GodMode.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GodMode : MonoBehaviour
+{
+    public GameObject player;
+    public Text enabledText;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setGodMode()
+    {
+        if(player.GetComponent<CircleCollider2D>().isTrigger == false)
+        {
+            player.GetComponent<CircleCollider2D>().isTrigger = true;
+            enabledText.text = "ON";
+        }
+        else if (player.GetComponent<CircleCollider2D>().isTrigger == true)
+        {
+            player.GetComponent<CircleCollider2D>().isTrigger = false;
+            enabledText.text = "OFF";
+        }
+        
+    }
+}
